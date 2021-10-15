da9f412fa351160b1a4d20c54b2a81c3e9170404
Michiel <michiel1.peeters@gmail.com>
2021-10-15 16:50:50 +0200

    practice voor examen 5_deelbaar_door.php done

---
+++ examen/5_deelbaar_door.php
@@ -0,0 +1,23 @@
+<?php
+
+$deler =7;
+
+for ($i = 0; $i < 20; $i++) {
+    $getal = random_int(100, 999);
+
+    if (DeelbaarDoor($getal, $deler)){
+        print "$getal is deelbaar door $deler\n";
+    }
+    else{
+        print "$getal\n";
+    }
+}
+
+function DeelbaarDoor($getal, $deler)
+{
+    if ($getal % $deler == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
